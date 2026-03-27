39d948856b6ffc9e195af732676616e9b9572fd8
lucasviniandrade422-lab <lucasviniandrade422@gmail.com>
2026-03-27 15:22:38 -0300

    Update biblioteca.py

--- src/backend/biblioteca.py
+++ src/backend/biblioteca.py
@@ -1,1 +1,31 @@
+from usuario import Usuario
+from livro import Livro
 
+class Biblioteca:
+    def __init__(self):
+        self.usuarios = []
+        self.livros = []
+
+    def cadastrar_usuario(self, nome, email):
+        usuario = Usuario(nome, email)
+        self.usuarios.append(usuario)
+        print("Usuário cadastrado com sucesso!")
+
+    def cadastrar_livro(self, titulo, autor):
+        livro = Livro(titulo, autor)
+        self.livros.append(livro)
+        print("Livro cadastrado com sucesso!")
+
+    def listar_livros(self):
+        for livro in self.livros:
+            print(livro)
+
+    def emprestar_livro(self, titulo):
+        for livro in self.livros:
+            if livro.titulo == titulo:
+                if livro.emprestar():
+                    print("Livro emprestado com sucesso!")
+                else:
+                    print("Livro já está emprestado.")
+                return
+        print("Livro não encontrado.")
