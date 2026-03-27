7537c8b10ee94ecba93b1598bb5be475932688f1
lucasviniandrade422-lab <lucasviniandrade422@gmail.com>
2026-03-27 15:21:32 -0300

    Update usuario.py

--- src/backend/usuario.py
+++ src/backend/usuario.py
@@ -1,1 +1,7 @@
+class Usuario:
+    def __init__(self, nome, email):
+        self.nome = nome
+        self.email = email
 
+    def __str__(self):
+        return f"Usuário: {self.nome} - {self.email}"
